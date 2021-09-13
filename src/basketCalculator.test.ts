7e3f0b261f1014cc940cec4a5f4388bb9ca7f5cf
Gary Roberts <tallstreet@gmail.com>
2021-09-13 18:16:42 +0100

    Return individual line items

--- src/basketCalculator.test.ts
+++ src/basketCalculator.test.ts
@@ -14,3 +14,15 @@ test("calculates the subtotal for a basket", () => {
   const { subTotal } = sum(ITEMS, EXAMPLE_BASKET);
   expect(subTotal).toBe("3.29");
 });
+
+test("returns line items", () => {
+  const { lineItems } = sum(ITEMS, EXAMPLE_BASKET);
+  expect(lineItems.length).toBe(6);
+  expect(lineItems[0].name).toBe("Beans");
+  expect(lineItems[0].price).toBe("0.50");
+  expect(lineItems[3].name).toBe("Cola");
+  expect(lineItems[3].price).toBe("0.70");
+  expect(lineItems[5].name).toBe("Oranges");
+  expect(lineItems[5].quantity).toBe("0.195 kg @ £1.99/kg");
+  expect(lineItems[5].price).toBe("0.39");
+});
--- src/basketCalculator.ts
+++ src/basketCalculator.ts
@@ -2,6 +2,11 @@ import { IBasket, ItemData } from "./data";
 
 export interface BasketTotal {
   subTotal: string;
+  lineItems: {
+    name: string;
+    price: string;
+    quantity?: string;
+  }[];
 }
 
 export function displayNum(num: number) {
@@ -13,8 +18,20 @@ export function sum(items: ItemData, basket: IBasket): BasketTotal {
     (acc, val) => items[val.itemId].price * val.quantity + acc,
     0
   );
+  const lineItems = basket.map((val) => {
+    return {
+      name: items[val.itemId].itemName,
+      price: displayNum(items[val.itemId].price * val.quantity),
+      quantity: items[val.itemId].unit
+        ? `${val.quantity} ${items[val.itemId].unit} @ £${displayNum(
+            items[val.itemId].price
+          )}/${items[val.itemId].unit}`
+        : undefined,
+    };
+  });
 
   return {
     subTotal: displayNum(subTotal),
+    lineItems,
   };
 }
