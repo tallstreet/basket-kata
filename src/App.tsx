8b7f187b325453cc0db5730a80995ad84429f879
Gary Roberts <tallstreet@gmail.com>
2021-09-13 19:05:50 +0100

    Remove example basket add ability to add to a basket in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import "./App.css";
 import { Basket } from "./Basket";
 import { sum } from "./basketCalculator";
-import { COUPONS, ITEMS } from "./data";
+import { COUPONS, IBasket, ItemId, ITEMS } from "./data";
 import { Items } from "./Items";
 
-const EXAMPLE_BASKET = [
-  { itemId: "1", quantity: 1 },
-  { itemId: "1", quantity: 1 },
-  { itemId: "1", quantity: 1 },
-  { itemId: "2", quantity: 1 },
-  { itemId: "2", quantity: 1 },
-  { itemId: "3", quantity: 0.195 },
-];
-
 function App() {
-  const basketTotals = sum(ITEMS, EXAMPLE_BASKET, COUPONS);
-  const onAdd = () => undefined;
+  const [basket, setBasket] = useState<IBasket>([]);
+
+  const onAdd = useCallback(
+    (itemId: ItemId, quantity: number) => {
+      setBasket([...basket, { itemId: itemId, quantity }]);
+    },
+    [setBasket, basket]
+  );
+
+  let basketTotals = useMemo(() => {
+    return sum(ITEMS, basket, COUPONS);
+  }, [basket]);
+
   return (
     <div className="App">
       <Items items={ITEMS} onAdd={onAdd} />
